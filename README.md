dd58e7d0a2e78c3ec01d52c4930c2bc2fda4b789
Chase Lowney <chasea2016@gmail.com>
2023-02-13 10:15:10 -0500

    generates decent output, but is warped vertically

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Generating ASCII Art
 this program uses a reference image to generate ascii art
+
+as of right now it seems that the image is being warped vertically in the representation and the line I put in there to stop that (lines 26-27) aren't doing what they are supposed to
--- ascii_art_generator.py
+++ ascii_art_generator.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
 #opening the reference image
-reference = "mustache.jpg"
-reference_path = f"reference_images/{reference}"
+reference_name = "mustache"
+reference_path = f"reference_images/{reference_name}.jpg"
 
 reference_image = Image.open(reference_path)
 reference_pixels = list(reference_image.getdata())
@@ -10,12 +10,30 @@ reference_size = reference_image.size # getting the refence image's size in (x-p
 
 
 # making a greyscale version of the reference image
-reference_greyscale = []
+reference = []
 for p in reference_pixels:
     avg = (p[0]+p[1]+p[2])/3
-    reference_greyscale.append((avg,avg,avg))
+    reference.append((avg,avg,avg))
 
-# defining the ascii characters that correspond to certain 'levels of grey' going from white --> black
+# defining the ascii characters that correspond to certain 'levels of grey' going from black --> white
 raw = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
-ascii_sorted = [x for x in raw[::-1]]
+ascii_sorted = [x for x in raw]
+
+output = """"""
+index = 0
+for pixel in reference:
+    # figuring out what line of the image we are on (we are only going to do every other vertical line)
+    line = index % reference_size[0]
+    if line % 2==0:
+        if index % reference_size[0] == 0:
+            output += "\n"
+        else:
+            shade = pixel[0]
+            output += ascii_sorted[round(shade/3.643)-1]
+            
+    index += 1
+
+# writing the output to a text file
+with open(f"output_{reference_name}.txt","w") as f:
+    f.write(output)
 
